876d30c0b941f732f5321d7e3a7dbac0e504b92b
Toukheer Baig <touqeerbaig05@gmail.com>
2023-04-06 12:31:06 +0530

    initial commit: started with given code and made minor changes

---
+++ microservices/landing/app/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+import requests
+import os
+
+app = Flask(__name__)
+app.secret_key = 'thisisjustarandomstring'
+
+
+def add(n1, n2):
+    return n1+n2
+
+def minus(n1, n2):
+    return n1-n2
+
+def multiply(n1, n2):
+    return n1*n2
+
+def divide(n1, n2):
+    return n1/n2
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    number_1 = request.form.get('first')
+    number_2 = request.form.get('second')
+    operation = request.form.get('operation')
+
+    result = 0
+    if operation == 'add':
+        result = add(int(number_1), int(number_2))
+    elif operation == 'minus':
+        result =  minus(int(number_1), int(number_2))
+    elif operation == 'multiply':
+        result = multiply(int(number_1), int(number_2))
+    elif operation == 'divide':
+        result = divide(int(number_1), int(number_2))
+
+    flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=5051,
+        host="0.0.0.0"
+    )
