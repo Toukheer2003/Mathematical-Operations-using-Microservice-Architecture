df3aee65a438987e822749214e7ecd01585b9edf
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2023-04-14 20:13:23 +0530

    Add files via upload

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -1,50 +1,3 @@
-# from flask import Flask, render_template, request, flash, redirect, url_for
-
-# import requests
-# import os
-
-# app = Flask(__name__)
-# app.secret_key = 'thisisjustarandomstring'
-
-
-# def add(n1, n2):
-#     return n1+n2
-
-# def minus(n1, n2):
-#     return n1-n2
-
-# def multiply(n1, n2):
-#     return n1*n2
-
-# def divide(n1, n2):
-#     return n1/n2
-
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#     number_1 = request.form.get("first")
-#     number_2 = request.form.get('second')
-#     operation = request.form.get('operation')
-#     result = 0
-#     if operation == 'add':
-#         result = add(int(number_1), int(number_2))
-#     elif operation == 'minus':
-#         result =  minus(int(number_1), int(number_2))
-#     elif operation == 'multiply':
-#         result = multiply(int(number_1), int(number_2))
-#     elif operation == 'divide':
-#         result = divide(int(number_1), int(number_2))
-
-#     flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(
-#         debug=True,
-#         port=5051,
-#         host="0.0.0.0"
-#     )
-
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 import requests
@@ -55,54 +8,87 @@ app.secret_key = 'thisisjustarandomstring'
 
 
 def add(n1, n2):
-    return n1+n2
+    url = "http://addition-service:5052/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1+n2
 
 def minus(n1, n2):
-    return n1-n2
+    url = "http://subtraction-service:5053/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1-n2
 
 def multiply(n1, n2):
-    return n1*n2
+    url = "http://multiplication-service:5054/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1*n2
 
 def divide(n1, n2):
-    try:
-        return n1/n2
-    except:
-        flash("No division by zero")
+    url = "http://division-service:5055/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1/n2
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    number_1 = request.form.get("first")
+    number_1 = request.form.get('first')
     number_2 = request.form.get('second')
     operation = request.form.get('operation')
+
     result = 0
+
     if operation == 'add':
-        if(number_1=="" or number_2==""):
-            flash("Give valid Numbers")
+        if((number_1=="" or number_2=="")):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+
         else:
             result = add(int(number_1), int(number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+
+
     elif operation == 'minus':
-        if(number_1=="" or number_2==""):
-            flash("Give valid Numbers")
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+
         else:
             result =  minus(int(number_1), int(number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+
     elif operation == 'multiply':
-        if(number_1=="" or number_2==""):
-            flash("Give valid Numbers")
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+
         else:
             result = multiply(int(number_1), int(number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+  
+    
     elif operation == 'divide':
-        if(number_1=="" or number_2==""):
-            flash("Give valid Numbers")
-        else:
-            if(number_2==0):
-                flash("Number 2 cannot be zero...")
-            else:
-                result = divide(int(number_1), int(number_2))
-                flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+        
+        elif(number_2=="0"):
+            flash(f'Zero Division Errorrrr...')
 
+        else:
+            result = divide(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
     return render_template('index.html')
 
