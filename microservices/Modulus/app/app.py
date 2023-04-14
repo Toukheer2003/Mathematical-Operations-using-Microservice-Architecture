78f5f1479fe9f43a8e296eba29365dff7d405d26
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2023-04-14 23:51:17 +0530

    Add files via upload

--- microservices/Modulus/app/app.py
+++ microservices/Modulus/app/app.py
@@ -3,7 +3,11 @@ from flask_restful import Resource,Api
 
 class Modulus(Resource): 
 	def get(self, number_1, number_2):
-		return {'Output': (int(number_1) % int(number_2))}
+		try:
+			res=int(number_1) % int(number_2)
+			return {'Output': (res)}
+		except ZeroDivisionError:
+			return {'Output':"Error: Modulo by Zero"}
 
 app = Flask(__name__)
 api = Api(app)
