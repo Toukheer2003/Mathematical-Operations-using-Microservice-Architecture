cd83617011174d7843287f9afb2980d1ed3b6277
Toukheer2003 <106578809+Toukheer2003@users.noreply.github.com>
2023-04-18 09:23:39 +0530

    Update app.py

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -1,3 +1,20 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+1 of 2,383
+(no subject)
+Inbox
+
+HRISHIKESH BHAT
+Attachments
+9:22 AM (0 minutes ago)
+to me
+
+
+ One attachment
+  •  Scanned by Gmail
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 import requests
@@ -20,10 +37,28 @@ def minus(n1, n2):
     # return n1-n2
 
 def multiply(n1, n2):
-    return n1*n2
+    url = "http://multiplication-service:5054/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1*n2
 
 def divide(n1, n2):
-    return n1/n2
+        url = "http://division-service:5055/"+str(n1)+"/"+str(n2)
+        response = requests.get(url)
+        return response.json()['Output']
+    # return n1/n2
+    
+def exponent(n1, n2):
+    url = "http://exponent-service:5056/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1**n2
+    
+def modulus(n1, n2):
+    url = "http://modulus-service:5057/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1%n2
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -34,55 +69,78 @@ def index():
     result = 0
 
     if operation == 'add':
-        if((number_1=="" or number_2=="")):
-            flash(f'Please enter the numbers properly...')
-            
-        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
-            flash(f'Please enter the numbers properly...')
-
-        else:
-            result = add(int(number_1), int(number_2))
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = add((number_1), (number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        except ValueError as e:
+            flash(f'Please enter the numbers properly...')
 
 
 
     elif operation == 'minus':
-        if((number_1==None or number_2==None)):
-            flash(f'Please enter the numbers properly...')
-            
-        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
-            flash(f'Please enter the numbers properly...')
-
-        else:
-            result =  minus(int(number_1), int(number_2))
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = minus((number_1), (number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        except ValueError as e:
+            # flash(e)
+            flash(f'Please enter the numbers properly...')
 
 
     elif operation == 'multiply':
-        if((number_1==None or number_2==None)):
-            flash(f'Please enter the numbers properly...')
-            
-        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = multiply((number_1), (number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        except ValueError as e:
             flash(f'Please enter the numbers properly...')
 
-        else:
-            result = multiply(int(number_1), int(number_2))
-            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-  
     
     elif operation == 'divide':
-        if((number_1==None or number_2==None)):
-            flash(f'Please enter the numbers properly...')
-            
-        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
-            flash(f'Please enter the numbers properly...')
-        
-        elif(number_2=="0"):
-            flash(f'Zero Division Errorrrr...')
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = divide((number_1), (number_2))
+            if(result=="Division by Zero error"):
+                flash("Error: Division by Zero")
+            else:
+                flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
-        else:
-            result = divide(int(number_1), int(number_2))
+        except ValueError as e:
+            flash(f'Please enter the numbers properly...')
+       
+    
+    elif operation == 'exponent':
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = exponent((number_1), (number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        except ValueError as e:
+            # flash(e)
+            flash(f'Please enter the numbers properly...')
+    
+    
+    elif operation == 'modulus':
+        try:
+            number_1=float(number_1)
+            number_2=float(number_2)
+            result = modulus((number_1), (number_2))
+            if(result=="Modulo by Zero error"):
+                flash("Error: Modulo by Zero error")
+            else:
+                flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+
+        except ValueError as e:
+            flash(f'Please enter the numbers properly...')
 
     return render_template('index.html')
 
@@ -92,3 +150,5 @@ if __name__ == '__main__':
         port=5051,
         host="0.0.0.0"
     )
+app.py
+Displaying app.py.
