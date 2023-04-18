3d4ac0d25f1223bdeabc2386f59fd33da7ef8c10
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2023-04-18 10:34:19 +0530

    Final Commit
    Contains 7 functionalities with boundary conditions

--- microservices/Equals/app/app.py
+++ microservices/Equals/app/app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource,Api
 
 class equals(Resource): 
 	def get(self, number_1, number_2):
-		return {'Output': (int(number_1) == int(number_2))}
+		return {'Output': (float(number_1) == float(number_2))}
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +12,6 @@ api.add_resource(equals, '/<number_1>/<number_2>')
 if __name__ =="__main__":
 	app.run(
 		debug=True,
-		port=5059,
+		port=5058,
 		host="0.0.0.0"
 	)
