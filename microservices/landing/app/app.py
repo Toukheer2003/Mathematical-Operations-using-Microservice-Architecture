d1e768901b6b817277b0fb05ddc022af4ac1c541
Toukheer2003 <106578809+Toukheer2003@users.noreply.github.com>
2023-04-18 10:17:11 +0530

    Update app.py
    updated landing app.py

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -26,9 +26,9 @@ def multiply(n1, n2):
     # return n1*n2
 
 def divide(n1, n2):
-        url = "http://division-service:5055/"+str(n1)+"/"+str(n2)
-        response = requests.get(url)
-        return response.json()['Output']
+    url = "http://division-service:5055/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
     # return n1/n2
     
 def exponent(n1, n2):
@@ -43,6 +43,12 @@ def modulus(n1, n2):
     return response.json()['Output']
     # return n1%n2
 
+def equals(n1, n2):
+    url = "http://equals-service:5059/"+str(n1)+"/"+str(n2)
+    response = requests.get(url)
+    return response.json()['Output']
+    # return n1%n2
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     number_1 = request.form.get('first')
@@ -52,78 +58,97 @@ def index():
     result = 0
 
     if operation == 'add':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = add((number_1), (number_2))
-            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-
-        except ValueError as e:
+        if((number_1=="" or number_2=="")):
             flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+
+        else:
+            result = add(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
 
 
     elif operation == 'minus':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = minus((number_1), (number_2))
-            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-
-        except ValueError as e:
-            # flash(e)
+        if((number_1==None or number_2==None)):
             flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+
+        else:
+            result =  minus(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
 
     elif operation == 'multiply':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = multiply((number_1), (number_2))
-            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-
-        except ValueError as e:
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
             flash(f'Please enter the numbers properly...')
 
+        else:
+            result = multiply(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+  
     
     elif operation == 'divide':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = divide((number_1), (number_2))
-            if(result=="Division by Zero error"):
-                flash("Error: Division by Zero")
-            else:
-                flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
-
-        except ValueError as e:
+        if((number_1==None or number_2==None)):
             flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+        
+        elif(number_2=="0"):
+            flash(f'Zero Division Errorrrr...')
+
+        else:
+            result = divide(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
        
     
     elif operation == 'exponent':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = exponent((number_1), (number_2))
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly1...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly2...')
+
+        else:
+            result = exponent(int(number_1), int(number_2))
+            flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+        
+        
+    elif operation == 'modulus':
+        if((number_1==None or number_2==None)):
+            flash(f'Please enter the numbers properly...')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
+        
+        elif(number_2=="0"):
+            flash(f'Modulus by Zero is undefined..')
+        
+        else:
+            result = modulus(int(number_1), int(number_2))
             flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
-        except ValueError as e:
-            # flash(e)
+    elif operation == 'equals':
+        if((number_1==None or number_2==None)):
             flash(f'Please enter the numbers properly...')
-    
-    
-    elif operation == 'modulus':
-        try:
-            number_1=float(number_1)
-            number_2=float(number_2)
-            result = modulus((number_1), (number_2))
-            if(result=="Modulo by Zero error"):
-                flash("Error: Modulo by Zero error")
-            else:
-                flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
+            
+        elif(number_1.isdigit()!=True or number_2.isdigit()!=True):
+            flash(f'Please enter the numbers properly...')
 
-        except ValueError as e:
-            flash(f'Please enter the numbers properly...')
+        
+        else:
+            result = equals(int(number_1), int(number_2))
+            if result==True:
+                flash(f'Both  {number_1} and {number_2} are equal')
+            else:    
+                flash(f'Both  {number_1} and {number_2} are not equal')        
 
     return render_template('index.html')
 
